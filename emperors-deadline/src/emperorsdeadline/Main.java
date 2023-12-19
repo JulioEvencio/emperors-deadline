fee3550f97a547ced66bd093cc79406de4267862
JulioEvencio <julioigreja.dev@gmail.com>
2023-12-19 00:32:32 -0300

    added: game base

--- emperors-deadline/src/emperorsdeadline/Main.java
+++ emperors-deadline/src/emperorsdeadline/Main.java
@@ -1,9 +1,27 @@
 package emperorsdeadline;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import emperorsdeadline.strings.StringGame;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		SwingUtilities.invokeLater(() -> {
+			JFrame frame = new JFrame();
+			Game game = new Game();
+
+			frame.setTitle(StringGame.TITLE);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.add(game);
+			frame.setResizable(false);
+			frame.pack();
+			frame.setLocationRelativeTo(null);
+			frame.setVisible(true);
+
+			new Thread(game).start();
+		});
 	}
 
 }
---
+++ emperors-deadline/src/emperorsdeadline/strings/StringGame.java
@@ -0,0 +1,7 @@
+package emperorsdeadline.strings;
+
+public class StringGame {
+
+	public static final String TITLE = "Emperor's Deadline";
+
+}
