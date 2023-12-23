c6c1a3f49000a001f4fc0cf5df434baae43776c3
JulioEvencio <julioigreja.dev@gmail.com>
2023-12-23 03:13:40 -0300

    version 1.0

--- emperors-deadline/src/emperorsdeadline/Game.java
+++ emperors-deadline/src/emperorsdeadline/Game.java
@@ -26,7 +26,7 @@ public class Game extends Canvas implements Runnable, KeyListener, MouseListener
 
 	private static final long serialVersionUID = 1L;
 
-	public static final String VERSION = "0.1";
+	public static final String VERSION = "1.0";
 
 	public static final int SCALE = 1;
 	public static final int WIDTH = 720;
